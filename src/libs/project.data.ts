f85e5cf7c3643013055d61effbe9b3b3bc341bb6
vk196 <victorumeh196@gmail.com>
2023-09-08 23:18:20 +0100

    build

---
+++ src/libs/project.data.ts
@@ -0,0 +1,42 @@
+const projects = [
+  {
+    name: "Phony Gad",
+    description: "An Ecommerce store",
+    live: "https://phony-gad-redux-toolkit.vercel.app/",
+    technologies: "Javascript + React + Css + Redux-Toolkit",
+    github: "https://github.com/Victor-Umeh/phony-gad-redux-toolkit",
+  },
+
+  {
+    name: "Tip Calculator",
+    description: "A frontend mentor tip calculator app",
+    live: "https://tip-calculator-app-main-omega.vercel.app/",
+    technologies: "html + sass + javascript",
+    github:
+      "https://github.com/Victor-Umeh/Tip-Calculator-App-Main/tree/main/tip-calculator-app-main",
+  },
+
+  {
+    name: "Calculator",
+    description: "A frontend mentor calculator app with theme switcher",
+    live: "https://fem-calculator-app-roan.vercel.app/",
+    technologies: "html + css + javascript",
+    github: "https://github.com/Victor-Umeh/fem-calculator-app",
+  },
+
+  {
+    name: "",
+    description: "",
+    live: "",
+    technologies: "",
+    github: "",
+  },
+
+  {
+    name: "",
+    description: "",
+    live: "",
+    technologies: "",
+    github: "",
+  },
+];
